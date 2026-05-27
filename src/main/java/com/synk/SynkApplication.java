7bb35d79a2f0f178bd62cfab43f9faff55bc5dbd
유현 <cadry238@gmail.com>
2026-05-27 10:16:31 +0900

    Initial commit

---
+++ src/main/java/com/synk/SynkApplication.java
@@ -0,0 +1,13 @@
+package com.synk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynkApplication.class, args);
+	}
+
+}
